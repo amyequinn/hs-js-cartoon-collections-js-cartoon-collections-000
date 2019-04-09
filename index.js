2bf17d13846e06bfa017888e9232b9052eb4ca20
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-04-09 15:15:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function dwarfRollCall(dwarves) {
+
+function dwarfRollCall(dwarves){
+	var newString = "";
+	dwarves.forEach(function(name, index){
+		newString += (index+1 + ". " +name+ " ");
+	});
+
+	return newString;
 }
+dwarfRollCall(["Doc", "Dopey", "Bashful", "Grumpy"]);
+
 
 function summonCaptainPlanet(planeteerCalls){
+var newArr = [];
+planeteerCalls.forEach(function(calls) {
+calls = calls.toUpperCase()+"!";
+newArr.push(calls);
+});
+return newArr;
+}
+summonCaptainPlanet(["earth", "wind", "fire", "water", "heart"]);
+
+
+function longPlaneteerCalls(arrCalls){
+	for (var i = 0; i < arrCalls.length; i++){
+	if (arrCalls[i].length>4){
+		return true;
+	}
+	}
+return false;
 }
 
-function longPlaneteerCalls(words) {
-}
+longPlaneteerCalls(["hello", "goodbye"]);
 
-function findTheCheese (foods) {
+
+
+function findTheCheese(items){
+
+var cheese = ["cheddar", "gouda", "camembert"];
+
+for (var i = 0; i<items.length; i++){
+	var cheesePresent = cheese.indexOf(items[i]);
+	if(cheesePresent !== -1){
+		return items[i];
+	}
 }
+		return "no cheese!";
+	}
+
+findTheCheese(["butter", "jam", "camembert", "gouda"]);
